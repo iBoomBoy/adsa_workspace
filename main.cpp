1a2f91e7e427301f476c6a1a07a851dcc0100292
Ajay Ramesh <105428423+iBoomBoy@users.noreply.github.com>
2025-11-02 12:26:33 +1030

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,80 +1,72 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int cost(char c) {
-    if (c >= 'A' && c <= 'Z') return c - 'A';
-    else return c - 'a' + 26;
+int letterCost(char c){
+    if('A'<=c && c<='Z') return c - 'A';
+    return c - 'a' + 26;
 }
 
-struct Edge {
+struct Edge{
     int u, v, w;
-    bool exist;
-    Edge(int _u, int _v, int _w, bool _exist) : u(_u), v(_v), w(_w), exist(_exist) {}
+    Edge(int _u,int _v,int _w):u(_u),v(_v),w(_w){}
 };
 
-int findParent(vector<int>& parent, int x) {
-    if (parent[x] == x) return x;
-    return parent[x] = findParent(parent, parent[x]);
-}
-
-bool unionSet(vector<int>& parent, vector<int>& rank, int a, int b) {
-    a = findParent(parent, a);
-    b = findParent(parent, b);
-    if (a == b) return false;
-    if (rank[a] < rank[b]) swap(a, b);
-    parent[b] = a;
-    if (rank[a] == rank[b]) rank[a]++;
+int findp(vector<int>& p,int x){ return p[x]==x? x : p[x]=findp(p,p[x]); }
+bool unite(vector<int>& p, vector<int>& r, int a,int b){
+    a = findp(p,a); b = findp(p,b);
+    if(a==b) return false;
+    if(r[a]<r[b]) swap(a,b);
+    p[b]=a;
+    if(r[a]==r[b]) r[a]++;
     return true;
 }
 
-int main() {
+int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    string countryInput, buildInput, destroyInput;
-    cin >> countryInput >> buildInput >> destroyInput;
+    string countryStr, buildStr, destroyStr;
+    if(!(cin >> countryStr >> buildStr >> destroyStr)) return 0;
 
-    vector<string> countryParts, buildParts, destroyParts;
-    stringstream sc(countryInput), sb(buildInput), sd(destroyInput);
-    string temp;
-    while (getline(sc, temp, ',')) countryParts.push_back(temp);
-    while (getline(sb, temp, ',')) buildParts.push_back(temp);
-    while (getline(sd, temp, ',')) destroyParts.push_back(temp);
+    vector<string> country, build, destroy;
+    {
+        string tmp; stringstream sc(countryStr), sb(buildStr), sd(destroyStr);
+        while(getline(sc,tmp,',')) country.push_back(tmp);
+        while(getline(sb,tmp,',')) build.push_back(tmp);
+        while(getline(sd,tmp,',')) destroy.push_back(tmp);
+    }
+    int n = (int)country.size();
 
-    int n = countryParts.size();
-    vector<Edge> edges;
-    int totalDestroy = 0;
+    long long S = 0;
+    vector<Edge> edges; edges.reserve(n*(n-1)/2);
 
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (countryParts[i][j] == '1') {
-                edges.emplace_back(i, j, cost(destroyParts[i][j]), true);
-                totalDestroy += cost(destroyParts[i][j]);
-            } else {
-                edges.emplace_back(i, j, cost(buildParts[i][j]), false);
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(country[i][j]=='1'){
+                int d = letterCost(destroy[i][j]);
+                S += d;
+                edges.emplace_back(i, j, -d);
+            }else{
+                int b = letterCost(build[i][j]);
+                edges.emplace_back(i, j, +b);
             }
         }
     }
 
-    sort(edges.begin(), edges.end(), [](const Edge& a, const Edge& b) {
+    sort(edges.begin(), edges.end(), [](const Edge& a, const Edge& b){
         return a.w < b.w;
     });
 
-    vector<int> parent(n), rank(n, 0);
+    vector<int> parent(n), rankv(n,0);
     iota(parent.begin(), parent.end(), 0);
 
-    int mstCost = 0;
-    int usedDestroy = 0;
-
-    for (auto& e : edges) {
-        if (unionSet(parent, rank, e.u, e.v)) {
-            if (e.exist)
-                usedDestroy += e.w;
-            else
-                mstCost += e.w;
+    long long mst_weight = 0;
+    for(const auto& e : edges){
+        if(unite(parent, rankv, e.u, e.v)){
+            mst_weight += e.w;
         }
     }
 
-    cout << totalDestroy - usedDestroy + mstCost << "\n";
+    cout << (S + mst_weight) << "\n";
     return 0;
 }
